61fdc54fc1a48df8ba985541d34e9984dca0d1a2
NESKT <contactofabianmartinez@gmail.com>
2022-06-17 13:04:38 -0500

    more changes

--- js/script.js
+++ js/script.js
@@ -20,4 +20,27 @@ themeToggler.addEventListener('click', () => {
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+
+    //Guardar modo en localstorage.
+
+    if (document.body.classList.contains('dark-theme-variables')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+//Obetener el modo actual
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(2)').classList.add('active');
+    themeToggler.querySelector('span:nth-child(1)').classList.remove('active');
+
+
+} else {
+    document.body.classList.remove('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(1)').classList.add('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.remove('active');
+
+    
+}
